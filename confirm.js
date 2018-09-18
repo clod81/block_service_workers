cf4dcd60cabb06228b4c94a563bc79b23b3e32fe
Claudio Contin <claudio@zxsecurity.co.nz>
2018-09-19 08:49:01 +1200

    working on chrome

--- confirm.js
+++ confirm.js
@@ -1,8 +1,3 @@
-// chrome.storage.sync.get(null, function(items) {
-  // var allKeys = Object.keys(items);
-  // console.log(allKeys);
-// });
-
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
   if(request.message == "domain"){
     chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT}, function(tabs){
